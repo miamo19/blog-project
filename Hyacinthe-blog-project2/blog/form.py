2b75e445c63598518ad7c4dcc044d928d1331702
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-11-30 04:20:58 +0100

    Update form.py

--- Hyacinthe-blog-project2/blog/form.py
+++ Hyacinthe-blog-project2/blog/form.py
@@ -6,9 +6,9 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
-    username = forms.CharField(max_length=50, widget=TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(max_length=40, widget=TextInput(attrs={"class":"form-control"}))
     email    = forms.EmailField(widget=TextInput(attrs={"class": "form-control" }))
-    body     = forms.CharField(widget=Textarea(attrs={"class": "form-control",
+    body     = forms.CharField(max_length=70, widget=Textarea(attrs={"class": "form-control",
                                                       'rows':3,
                                                       "placeholder":"Join the discussion and leave a comment!"
                                                       }))
